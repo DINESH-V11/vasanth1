bb035729076bafc0cd51a4e7ad513d275e58c8ec
Vasanth <cs245214142@bhc.edu.in>
2025-12-10 10:51:23 +0530

    Add vasanth1.sh script

---
+++ vasanth1.sh
@@ -0,0 +1,12 @@
+# Simple Python program to add three numbers
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Calculating the sum
+sum = num1 + num2 + num3
+
+# Displaying the result
+print("The sum of the three numbers is:", sum)
